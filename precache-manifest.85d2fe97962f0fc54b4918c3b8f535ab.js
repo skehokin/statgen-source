52d208e506bea5b4ea5ac7ebb73e25539d556185
Siobhan Hokin <siobhan.hokin@rexsoftware.com.au>
2019-05-30 21:24:40 +1000

    Updates

---
+++ precache-manifest.85d2fe97962f0fc54b4918c3b8f535ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe44d6164179ceb4de72d7d73a10020",
+    "url": "/statgen/index.html"
+  },
+  {
+    "revision": "fbbeeafd05b5e0d54051",
+    "url": "/statgen/static/css/main.0d2c6ca2.chunk.css"
+  },
+  {
+    "revision": "7de46222cde73ac5a06a",
+    "url": "/statgen/static/js/2.71d0bafa.chunk.js"
+  },
+  {
+    "revision": "fbbeeafd05b5e0d54051",
+    "url": "/statgen/static/js/main.773609f7.chunk.js"
+  },
+  {
+    "revision": "e1c003d7de05414fc575",
+    "url": "/statgen/static/js/runtime~main.1eed20e1.js"
+  },
+  {
+    "revision": "1dcf77933dfd665e3c100e37523f982f",
+    "url": "/statgen/static/media/character_sheet.1dcf7793.pdf"
+  },
+  {
+    "revision": "13b95043019589f4d823dc4faf77af66",
+    "url": "/statgen/static/media/denny-muller-532466-unsplash5.13b95043.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/statgen/static/media/logo.5d5d9eef.svg"
+  }
+]);
